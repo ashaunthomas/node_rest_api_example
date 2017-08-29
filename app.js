95b47c50fc39672aeb8f6a1d4dadc8230a519d8e
Ashaun Thomas <ashaunthomas@siu.edu>
2017-08-29 11:14:16 -0500

    routing errors fixed (again), basic af UI

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-var express = require('express')
-    mongoose = require('mongoose');
-
+var express  = require("express");
+var mongoose = require("mongoose");
+var path = require("path");
+var Book = require('./models/bookModel');
 var app = express();
-var port = process.env.port || 3000; 
-var bookRouter = express.Router(); //router instance to define routes
-var db = mongoose.connect('mongodb://localhost/bookAPI');
-bookRouter.route('/Books')
-    .get(function(req, res) {
-        var responseJson = {hello: "This is my API"};
-        res.json(responseJson);
-    });
-
-app.use('/api', bookRouter);
-
-
-app.get('/', function(req, res) {
-    res.send("Welcome to my API");
+var port = process.env.PORT || 3000;
+var bookRouter = express.Router();
+var db = mongoose.connect('mongodb://localhost:27017/techpdfs', {
+    useMongoClient: true
 });
 
+app.use('/api',bookRouter);
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+});
 app.listen(port, function() {
     console.log("Gulp is running on port: " + port);
 });
 
+bookRouter.route('/books')
+    .get(function(req,res) {
+        Book.find(function(err,books) {
+            if (err) { console.log(err); }
+            else { res.json(books)}
+        });
+    });
